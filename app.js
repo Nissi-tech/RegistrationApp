acc6d8e033bba404c9934a236e3089700d938814
Nissi Lee <nissilee2607@gmail.com>
2026-07-16 12:06:58 +0800

    database cedentials updated

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const app = express();
 
 // Database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'RP738964$',
-    database: 'C237_usersdb'
+    host: 'c237-hannah-mysql.mysql.database.azure.com',
+    user: 'c237_024',
+    password: 'c237024@2026!',
+    database: 'C237_024_t3regapp'
 });
 
 db.connect((err) => {
